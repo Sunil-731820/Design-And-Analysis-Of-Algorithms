67c44ae9c8dae654cd0cce641ea50e35a3faca77
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-06-25 15:47:59 +0530

    Some Code Changes I Did

---
+++ src/main/java/com/java/DesignAndAnalysisOfAlgorithms/CompetitiveProgramming/HI.java
@@ -0,0 +1,8 @@
+package com.java.DesignAndAnalysisOfAlgorithms.CompetitiveProgramming;
+
+public class HI {
+	public static void main(String[] args) {
+		System.out.println("This is The Example Of The Competative programming ");
+	}
+
+}
