301e0186a43d41a9282cd130ce9d89914928079a
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-06-25 15:54:22 +0530

    code Done In Competative Array

---
+++ src/main/java/com/java/DesignAndAnalysisOfAlgorithms/CompetitiveProgramming/Top50ArrayProblems/FindAPeakElementWhichIsNotSmallerThanItsNeighbors.java
@@ -0,0 +1,8 @@
+package com.java.DesignAndAnalysisOfAlgorithms.CompetitiveProgramming.Top50ArrayProblems;
+
+public class FindAPeakElementWhichIsNotSmallerThanItsNeighbors {
+	public static void main(String[] args) {
+		System.out.println("This is The EXampe Of The FindAPeakElementWhichIsNotSmallerThanItsNeighbors");
+	}
+
+}
