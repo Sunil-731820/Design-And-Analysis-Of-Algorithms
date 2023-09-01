182d69261c4e4d4609dfe5ab1405d7da43dcfb2e
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-09-01 23:40:36 +0530

    Kth maximum value Probem Solution is Completed

--- src/main/java/allImportantDesignAndAnalysisOfAlgorithmsAndDS/AllArrayQuestions/FindTheKthMaxAndMinElementOfAnArray.java
+++ src/main/java/allImportantDesignAndAnalysisOfAlgorithmsAndDS/AllArrayQuestions/FindTheKthMaxAndMinElementOfAnArray.java
@@ -4,43 +4,43 @@ import java.util.Scanner;
 
 public class FindTheKthMaxAndMinElementOfAnArray {
 	
-	static void sortingTheData(int size , int[] numbers) {
+//	Sorting The Data Using Linear Sort
+	static void getSortingMethod(int size , int [] arr) {
 		for(int i=0;i<size;i++) {
 			for(int j= i+1;j<size;j++) {
-				if(numbers[i]>numbers[j]) {
-					int temp = numbers[i];
-					numbers[i] = numbers[j];
-					numbers[j] = temp;
+				if(arr[i]<arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
+		System.out.println("After Sorting the Data is ");
+		for(int i=0;i<size;i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 	
-	static void getTheKthMaxAndMinElementOfTheArray(int size , int numberOfArray[], int Kth) {
-		
-//		Sorting The Data 
-		sortingTheData(size, numberOfArray);
-		
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("This is the Example Of The Find the “Kth” max and min element of an array");
+	static void getKthMaximumvalue(int size , int [] arr) {
+		getSortingMethod(size, arr);
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The Size Of The Array : ");
+		System.out.println("Enter The Kth maximum value ");
+		int Kthmaximumvalue = sc.nextInt();
+		System.out.println("The kthe Maximum value is : "+ arr[Kthmaximumvalue-1]);
+	}
+	public static void main(String[] args) {
+		System.out.println("This is The Example of The finding the Kth maximum And kth Minimum value ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Size of The Array ");
 		int size = sc.nextInt();
-		int numberOfArray[] = new int[size];
+		int[] arr = new int[size];
 		for(int i=0;i<size;i++) {
-			System.out.println("Enter the Element in The Array :");
-			numberOfArray[i] = sc.nextInt();
+			System.out.println("Enter The Element In The Array is ");
+			arr[i] = sc.nextInt();
 		}
-		System.out.println("The Array ofThe Element is :");
-		for(int i=0;i<size;i++) {
-			System.out.print(numberOfArray[i] + " ");
-		}
-		System.out.println("Enter The Kth Element to find the Maximum Value ");
-		int kth = sc.nextInt();
-		System.out.println("The Kth Maximum value is : "+numberOfArray[kth]);
-		getTheKthMaxAndMinElementOfTheArray(size, numberOfArray, kth);
+		System.out.println("Before CalliNg The getKthMaximumvalue is : ");
+		getKthMaximumvalue(size, arr);
+		
 	}
 
 }
